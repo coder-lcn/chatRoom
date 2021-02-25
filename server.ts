dd14bbf05f654abe916c2b81bffbb14f27844c75
leechar_rem <3466435280@qq.com>
2021-02-26 05:00:30 +0800

    就这样吧

---
+++ server.ts
@@ -0,0 +1,30 @@
+import WebSocket, { Server } from 'ws'
+import http from 'http'
+
+class App {
+  server: Server;
+  port = 3000;
+  userList: WebSocket[] = []
+
+  constructor() {
+    this.init.call(this);
+  }
+
+  onMessage(msg: WebSocket.Data) {
+    this.userList.forEach((ws) => {
+      ws.send(msg);
+    });
+  }
+
+  onConnection(ws: WebSocket, req: http.IncomingMessage) {
+    this.userList.push(ws);
+    ws.on("message", (msg) => this.onMessage(msg));
+  }
+
+  init() {
+    this.server = new Server({ port: this.port });
+    this.server.on("connection", this.onConnection.bind(this));
+  }
+}
+
+new App();
